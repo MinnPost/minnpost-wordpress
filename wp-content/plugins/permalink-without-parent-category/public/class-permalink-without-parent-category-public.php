589b5945be808addfe1e19afb7105b12e2af8ca2
Jonathan Stegall <jstegall@minnpost.com>
2016-07-13 16:20:09 -0500

    apparently I updated the permalink plugin but never checked it in. I don't know what I was doing though, so maybe need to revert. But maybe not. ¯\_(ツ)_/¯

--- wp-content/plugins/permalink-without-parent-category/public/class-permalink-without-parent-category-public.php
+++ wp-content/plugins/permalink-without-parent-category/public/class-permalink-without-parent-category-public.php
@@ -55,22 +55,24 @@ class Permalink_Without_Parent_Category_Public {
 	}
 
 
-	public function remove_parent_cats_from_link( $permalink, $post, $leavename ) {
-		$cats = get_the_category( $post->ID );
-		if ( $cats ) {
-			// Make sure we use the same start cat as the permalink generator
-			usort( $cats, '_usort_terms_by_ID' ); // order by ID
-			$category = $cats[0]->slug;
-			if ( $parent = $cats[0]->parent ) {
-				// If there are parent categories, collect them and replace them in the link
-				$parentcats = get_category_parents( $parent, false, '/', true );
-				// str_replace() is not the best solution if you can have duplicates:
-				// myexamplesite.com/luxemburg/luxemburg/ will be stripped down to myexamplesite.com/
-				// But if you don't expect that, it should work
-				$permalink = str_replace( $parentcats, '', $permalink );
+	public function remove_parent_cats_from_link( $permalink, $post = '', $leavename = '' ) {
+		if ( $post !== '' ) {
+			$cats = get_the_category( $post->ID );
+			if ( $cats ) {
+				// Make sure we use the same start cat as the permalink generator
+				usort( $cats, '_usort_terms_by_ID' ); // order by ID
+				$category = $cats[0]->slug;
+				if ( $parent = $cats[0]->parent ) {
+					// If there are parent categories, collect them and replace them in the link
+					$parentcats = get_category_parents( $parent, false, '/', true );
+					// str_replace() is not the best solution if you can have duplicates:
+					// myexamplesite.com/luxemburg/luxemburg/ will be stripped down to myexamplesite.com/
+					// But if you don't expect that, it should work
+					$permalink = str_replace( $parentcats, '', $permalink );
+				}
 			}
+			return $permalink;
 		}
-		return $permalink;
 	}
 
 }
