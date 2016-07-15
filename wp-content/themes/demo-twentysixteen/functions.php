eab18110ba32081326cec0021a6f6453123d8801
Jonathan Stegall <jstegall@minnpost.com>
2016-07-15 09:11:21 -0500

    fix some thumbnail rendering in our demo theme

--- wp-content/themes/demo-twentysixteen/functions.php
+++ wp-content/themes/demo-twentysixteen/functions.php
@@ -20,3 +20,29 @@ function minnpost_demo_post_thumbnail_sizes_attr( $attr = array(), $attachment, 
 	return $attr;
 }
 add_filter( 'wp_get_attachment_image_attributes', 'minnpost_demo_post_thumbnail_sizes_attr', 10 , 3 );
+
+
+function twentysixteen_post_thumbnail() {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	if ( is_singular() ) :
+	?>
+
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail( 'detail' ); ?>
+	</div><!-- .post-thumbnail -->
+
+	<?php else : ?>
+
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+	<?php if ( is_home() ) : ?>
+		<?php the_post_thumbnail( 'thumbnail', array( 'alt' => the_title_attribute( 'echo=0' ) )); ?>
+	<?php else : ?>
+		<?php the_post_thumbnail( 'feature', array( 'alt' => the_title_attribute( 'echo=0' ) )); ?>
+	<?php endif; ?>
+	</a>
+
+	<?php endif; // End is_singular()
+}
