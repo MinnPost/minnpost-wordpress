1af0e00137396e3c2066f48fa5086b757bd51d18
Jonathan Stegall <jstegall@minnpost.com>
2016-08-25 16:36:57 -0500

    update mp salesforce plugin so that it works with cron as well as with instant sync

--- wp-content/plugins/minnpost-salesforce/minnpost-salesforce.php
+++ wp-content/plugins/minnpost-salesforce/minnpost-salesforce.php
@@ -34,10 +34,14 @@ class Minnpost_Salesforce {
 
 	public function salesforce() {
 		//get the base class
-		if ( is_plugin_active('salesforce-rest-api/salesforce-rest-api.php')  ) {
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+     		require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+     	}
+		if ( is_plugin_active('salesforce-rest-api/salesforce-rest-api.php') ) {
 			require_once plugin_dir_path( __FILE__ ) . '../salesforce-rest-api/salesforce-rest-api.php';
 			$salesforce = Salesforce_Rest_API::get_instance();
 			$this->salesforce = $salesforce;
+			return $this->salesforce;
 		}
 	}
 
@@ -64,18 +68,28 @@ class Minnpost_Salesforce {
 	*
 	*/
 	public function find_object_match( $salesforce_id, $wordpress_object ) {
+
+		if ( is_object( $this->salesforce ) ) {
+			$salesforce_api = $this->salesforce->salesforce['sfapi'];
+		} else {
+			$salesforce = $this->salesforce();
+			$salesforce_api = $salesforce->salesforce['sfapi'];
+		}
 		
-		$salesforce_api = $this->salesforce->salesforce['sfapi'];
+		if ( is_object( $salesforce_api ) ) {
 
-		// we want to see if the user's email address exists as a primary on any contact and use that contact if so
-		$mail = $wordpress_object['user_email'];
-		$query = "SELECT Primary_Contact__c FROM Email__c WHERE Email_Address__c = '$mail'";
-		$result = $salesforce_api->query( $query );
+			// we want to see if the user's email address exists as a primary on any contact and use that contact if so
+			$mail = $wordpress_object['user_email'];
+			$query = "SELECT Primary_Contact__c FROM Email__c WHERE Email_Address__c = '$mail'";
+			$result = $salesforce_api->query( $query );
 
-		if ( $result['data']['totalSize'] === 1 ) {
-			$salesforce_id = $result['data']['records'][0]['Primary_Contact__c'];
-		} else if ( $result['data']['totalSize'] > 1 ) {
-			error_log('Salesforce has ' . $result['data']['totalSize'] . ' matches for this email. Try to log all of them: ' . print_r($result['data']['records'], true));
+			if ( $result['data']['totalSize'] === 1 ) {
+				$salesforce_id = $result['data']['records'][0]['Primary_Contact__c'];
+			} else if ( $result['data']['totalSize'] > 1 ) {
+				error_log('Salesforce has ' . $result['data']['totalSize'] . ' matches for this email. Try to log all of them: ' . print_r($result['data']['records'], true));
+			}
+		} else {
+			error_log('object for sf api does not exist');
 		}
 
 		return $salesforce_id;
