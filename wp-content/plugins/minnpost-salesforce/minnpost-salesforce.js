7a1eac4450ee9dbb78398f15cce35518aa2d2c8a
Jonathan Stegall <jstegall@minnpost.com>
2017-01-11 11:04:39 -0600

    minnpost salesforce: fix styles so we can drop that thermometer demo in different places more easily

--- wp-content/plugins/minnpost-salesforce/minnpost-salesforce.js
+++ wp-content/plugins/minnpost-salesforce/minnpost-salesforce.js
@@ -1,31 +1,22 @@
 var $ = window.jQuery;
 function donationMeter() {
-    $('.total').css('bottom', $('.total').data('percent'));
-    $('.amount').animate({ height: $('.total').data('percent') + '%' }, 'slow');
-
-	if ($('.donation-meter').length > 0) {
-		// check to see if we should run it again
+    if ($('.donation-meter').length > 0) {
+	  	// check to see if we should run it again
 		if ( $('.total').text().length === 0) {
 			var data = {
 				report_id: $('.donation-meter').data('report'),
 				campaign_id: $('.donation-meter').data('campaign')
 			}
-			$.get(
-			    minnpost_salesforce.ajaxurl,
-			    {
-			        'action': 'thermometer_ajax',
-			        'data':   data
-			    }, 
-			    function(response) {
-			    	goal_int = response.data.goal;
+			$.get( minnpost_salesforce.ajaxurl, { 'action': 'thermometer_ajax', data: data } )
+				.done(function( response ) {
+					goal_int = response.data.goal;
 					third_int = goal_int / 3;
 					$('.goal').text('$' + Math.round(goal_int).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
 					$('.total').text('$' + Math.round(response.data.value_opportunities).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+					animateMeter(response.data.percent_complete);
 					$('.two-thirds').text('$' + Math.round(third_int * 2).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
 					$('.one-third').text('$' + Math.round(third_int).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-					animateMeter(response.data.percent_complete);
-			    }
-			);
+				});
 		} else {
 			var percent = $('.total').data('percent');
 			animateMeter(percent);
@@ -34,8 +25,8 @@ function donationMeter() {
 }
 
 function animateMeter(percent) {
-	$('.total').css('bottom', percent);
-	$('.amount').animate({ height: percent + '%' });
+	//$('.total').css('bottom', percent); // i think we don't need this but i don't know why it was here
+	$('.amount').animate({ height: percent + '%' }, 'slow');
 }
 
 $(document).ready(function() {
