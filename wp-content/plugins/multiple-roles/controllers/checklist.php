74064611f4bdf85e4458762adc6558da05768ccd
Jonathan Stegall <jstegall@minnpost.com>
2017-01-17 10:58:26 -0600

    this multiple roles plugin seems like a perfectly reasonable solution for handling that part of the permission question

---
+++ wp-content/plugins/multiple-roles/controllers/checklist.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * A set of action functions which handle the behavior of the roles checklist
+ * on user edit screens.
+ */
+class MDMR_Checklist_Controller {
+
+	/**
+	 * The model object.
+	 *
+	 * @var \MDMR_Model $model
+	 */
+	var $model;
+
+	/**
+	 * Constructor. Define properties.
+	 *
+	 * @param object $model The model object.
+	 */
+	public function __construct( $model ) {
+		$this->model = $model;
+	}
+
+	/**
+	 * Remove the default WordPress role dropdown from the DOM.
+	 *
+	 * @param string $hook The current admin screen.
+	 */
+	public function remove_dropdown( $hook ) {
+		if ( 'user-edit.php' !== $hook && 'user-new.php' !== $hook) {
+			return;
+		}
+		wp_enqueue_script( 'md-multiple-roles', MDMR_URL . 'views/js/scripts.js', array( 'jquery' ), '1.0' );
+	}
+
+	/**
+	 * Output the checklist view. If the user is not allowed to edit roles,
+	 * nothing will appear.
+	 *
+	 * @param object $user The current user object.
+	 */
+	public function output_checklist( $user ) {
+
+		if ( ! $this->model->can_update_roles() ) {
+			return;
+		}
+
+		wp_nonce_field( 'update-md-multiple-roles', 'md_multiple_roles_nonce' );
+
+		$roles        = $this->model->get_editable_roles();
+        $user_roles   = ( isset( $user->roles ) ) ? $user->roles : null;
+
+		include( apply_filters( 'mdmr_checklist_template', MDMR_PATH . 'views/checklist.html.php' ) );
+
+	}
+
+	/**
+	 * Update the given user's roles as long as we've passed the nonce
+	 * and permissions checks.
+	 *
+	 * @param int $user_id The user ID whose roles might get updated.
+	 */
+	public function process_checklist( $user_id ) {
+		if ( isset( $_POST['md_multiple_roles_nonce'] ) && ! wp_verify_nonce( $_POST['md_multiple_roles_nonce'], 'update-md-multiple-roles' ) ) {
+			return;
+		}
+
+		if ( ! $this->model->can_update_roles() ) {
+			return;
+		}
+
+		$new_roles = ( isset( $_POST['md_multiple_roles'] ) && is_array( $_POST['md_multiple_roles'] ) ) ? $_POST['md_multiple_roles'] : array();
+		if ( empty( $new_roles ) ) {
+			return;
+		}
+
+		$this->model->update_roles( $user_id, $new_roles );
+	}
+
+	/**
+	 * Add multiple roles in the $meta array in wp_signups db table
+	 *
+	 * @since 1.1.4
+	 *
+	 * @param $user
+	 * @param $user_email
+	 * @param $key
+	 * @param $meta
+	 */
+	public function mu_add_roles_in_signup_meta( $user, $user_email, $key, $meta ) {
+		if ( isset( $_POST['md_multiple_roles_nonce'] ) && ! wp_verify_nonce( $_POST['md_multiple_roles_nonce'], 'update-md-multiple-roles' ) ) {
+			return;
+		}
+
+		if ( ! $this->model->can_update_roles() ) {
+			return;
+		}
+
+		$new_roles = ( isset( $_POST['md_multiple_roles'] ) && is_array( $_POST['md_multiple_roles'] ) ) ? $_POST['md_multiple_roles'] : array();
+		if ( empty( $new_roles ) ) {
+			return;
+		}
+
+		global $wpdb;
+
+		// Get user signup
+		// Suppress errors in case the table doesn't exist
+		$suppress = $wpdb->suppress_errors();
+		$signup   = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->signups} WHERE user_email = %s", $user_email ) );
+		$wpdb->suppress_errors( $suppress );
+
+		if ( empty( $signup ) || is_wp_error( $signup ) ) {
+			return new WP_Error( 'md_get_user_signups_failed' );
+		}
+
+		// Add multiple roles to a new array in meta var
+		$meta = maybe_unserialize( $meta );
+		$meta['md_roles'] = $new_roles;
+		$meta = maybe_serialize( $meta );
+
+		// Update user signup with good meta
+		$where        = array( 'signup_id' => (int) $signup->signup_id );
+		$where_format = array( '%d' );
+		$formats      = array( '%s' );
+		$fields       = array( 'meta' => $meta );
+		$result       = $wpdb->update( $wpdb->signups, $fields, $where, $formats, $where_format );
+
+		// Check for errors
+		if ( empty( $result ) && ! empty( $wpdb->last_error ) ) {
+			return new WP_Error( 'md_update_user_signups_failed' );
+		}
+	}
+
+	/**
+	 * Add multiple roles after user activation
+	 *
+	 * @since 1.1.4
+	 *
+	 * @param $user_id
+	 * @param $password
+	 * @param $meta
+	 */
+	public function mu_add_roles_after_activation( $user_id, $password, $meta ) {
+		if ( ! empty( $meta['md_roles'] ) ) {
+			$this->model->update_roles( $user_id, $meta['md_roles'] );
+		}
+	}
+
+}
---
+++ wp-content/plugins/multiple-roles/multiple-roles.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: Multiple Roles
+Description: Allow users to have multiple roles on one site.
+Version: 1.1.4
+Author: Florian TIAR
+Author URI: http://tiar-florian.fr
+Plugin URI: https://wordpress.org/plugins/multiple-roles/
+Github URI: https://github.com/Mahjouba91/multiple-roles
+Text Domain: multiple-roles
+*/
+
+define( 'MDMR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'MDMR_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Load files and add hooks to get things rolling.
+ */
+require_once( MDMR_PATH . 'model.php' );
+require_once( MDMR_PATH . 'controllers/checklist.php' );
+require_once( MDMR_PATH . 'controllers/column.php' );
+
+$model = new MDMR_Model();
+
+$checklist = new MDMR_Checklist_Controller( $model );
+add_action( 'admin_enqueue_scripts', array( $checklist, 'remove_dropdown' ) );
+add_action( 'show_user_profile',     array( $checklist, 'output_checklist' ) );
+add_action( 'edit_user_profile',     array( $checklist, 'output_checklist' ) );
+add_action( 'user_new_form',         array( $checklist, 'output_checklist' ) );
+add_action( 'profile_update',        array( $checklist, 'process_checklist' ) );
+
+// For new user form (in Backoffice)
+// In multisite, user_register hook is too early so wp_mu_activate_user add user role after
+if ( is_multisite() ) {
+	add_action( 'after_signup_user',     array( $checklist, 'mu_add_roles_in_signup_meta' ), 10, 4 );
+	add_action( 'wpmu_activate_user',    array( $checklist, 'mu_add_roles_after_activation' ), 10, 3 );
+} else {
+	add_action( 'user_register',         array( $checklist, 'process_checklist' ) );
+}
+
+$column = new MDMR_Column_Controller( $model );
+add_filter( 'manage_users_columns',       array( $column, 'replace_column' ), 11 );
+add_filter( 'manage_users_custom_column', array( $column, 'output_column_content' ), 10, 3 );
+
+add_action( 'init', 'load_translation' );
+function load_translation() {
+	load_plugin_textdomain( 'multiple-roles', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
