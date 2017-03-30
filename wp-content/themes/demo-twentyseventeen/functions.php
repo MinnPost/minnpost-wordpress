bf276ea5bf7935ce582c6185dcccdd2933b56389
Jonathan Stegall <jstegall@minnpost.com>
2017-03-30 14:46:26 -0500

    edit the theme’s widgets so we can see the right names at least

--- wp-content/themes/demo-twentyseventeen/functions.php
+++ wp-content/themes/demo-twentyseventeen/functions.php
@@ -100,3 +100,60 @@ function twentyseventeen_posted_on() {
     endif;
 }
 endif;
+
+// Remove the default function
+function remove_twentyseventeen_widgets_init() {
+    // Unregister some of the TwentySeventeen sidebars
+	unregister_sidebar( 'sidebar-1' );
+	unregister_sidebar( 'sidebar-2' );
+	unregister_sidebar( 'sidebar-3' );
+}
+add_action( 'widgets_init', 'remove_twentyseventeen_widgets_init', 11 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function minnpost_twentyseventeen_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar Right', 'twentyseventeen' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your right sidebar.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Sidebar Middle', 'twentyseventeen' ),
+		'id'            => 'sidebar-2',
+		'description'   => __( 'Add widgets here to appear in your middle sidebar, which is more rarely used.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer Top', 'twentyseventeen' ),
+		'id'            => 'sidebar-3',
+		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer Bottom', 'twentyseventeen' ),
+		'id'            => 'sidebar-4',
+		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'minnpost_twentyseventeen_widgets_init', 20 );
