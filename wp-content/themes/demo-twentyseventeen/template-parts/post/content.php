c18ac248d1513a9b61884d5e09ed95e8b2aa94a5
Jonathan Stegall <jstegall@minnpost.com>
2017-04-04 16:42:39 -0500

    put the date/author in the example template

--- wp-content/themes/demo-twentyseventeen/template-parts/post/content.php
+++ wp-content/themes/demo-twentyseventeen/template-parts/post/content.php
@@ -20,23 +20,17 @@
 	?>
 	<header class="entry-header">
 		<?php
-			if ( 'post' === get_post_type() ) :
-				echo '<div class="entry-meta">';
-					if ( is_single() ) :
-						twentyseventeen_posted_on();
-					else :
-						echo twentyseventeen_time_link();
-						twentyseventeen_edit_link();
-					endif;
-				echo '</div><!-- .entry-meta -->';
-			endif;
-
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
 		?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">By <?php function_exists( 'coauthors_posts_links' ) ? coauthors_posts_links() : the_author_posts_link(); ?> | <?php echo is_single() ? twentyseventeen_time_link() : twentyseventeen_time_link() . twentyseventeen_edit_link() ?></div>
+		<?php endif; ?>
+
 	</header><!-- .entry-header -->
 
 	<?php if ( '' !== get_the_post_thumbnail() ) : ?>
@@ -64,8 +58,10 @@
 	<?php endif; ?>
 	
 	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
+
+		<?php if ( is_singular() ) : ?>
+
+			<?php
 			the_content( sprintf(
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
 				get_the_title()
@@ -77,7 +73,13 @@
 				'link_before' => '<span class="page-number">',
 				'link_after'  => '</span>',
 			) );
-		?>
+			?>
+
+		<?php else : ?>
+			<?php the_excerpt(); ?>
+		<?php endif; ?>
+
+		
 	</div><!-- .entry-content -->
 
 	<?php if ( is_single() ) : ?>
