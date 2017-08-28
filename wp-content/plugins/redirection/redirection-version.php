0ead55ddada8b372e89e74a2b3c4005e0a659b42
Jonathan Stegall <jstegall@minnpost.com>
2017-08-28 09:02:46 -0500

    update redirection plugin

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,4 +1,4 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '2.7.2' );
-define( 'REDIRECTION_BUILD', '71ffb43efe4d7e179530595e437485b3' );
+define( 'REDIRECTION_VERSION', '2.7.3' );
+define( 'REDIRECTION_BUILD', 'a27c77af0d47abb92ce2f83236f9986b' );
